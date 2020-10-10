d422718df76dd50d807e06c797918755051d5c95
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2020-10-10 16:10:02 -0400

    home page modified. Data, InfoArea and EndPointsArea imported

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,23 @@ import React, {Fragment, useState, useEffect} from "react";
 import View from "./view"; 
 
 const Home = (props)=>{
+
+    const [darkMode, setDarkMode] = useState(false);
+    const [dataUrl, setDataUrl] = useState("");
+
+    //componentDidMount
+    useEffect(()=>{
+
+    },[]);
+
+    //componentWillUnmount
+    useEffect(()=>{
+        return function(){
+
+        }
+    },[]);
+
+
     return(
         <Fragment>
             <View/>
--- src/pages/Home/view.js
+++ src/pages/Home/view.js
@@ -2,11 +2,19 @@ import React from "react";
 
 //styles
 import "./styles.css"; 
+//data
+import data from "../../data/infoAPI.json";
+//components
+import {InfoArea} from "../../components/InfoArea";
+import {EndPointsArea} from "../../components/EndPointsArea";
 
 export default function(props){
     return(
         <div className="ctr-home">
-            home
+            {console.log(data)}
+            <InfoArea title={data.title} description={data.description}/>
+            <EndPointsArea endpoints={data.end_points}/>
+           
         </div>
     );
 }
