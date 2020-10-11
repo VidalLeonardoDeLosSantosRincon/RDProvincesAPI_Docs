919d4c208ac2658fd58c7e515e35c29cdff9f39b
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2020-10-11 16:38:07 -0400

    DataSchema imported

--- src/pages/Home/view.js
+++ src/pages/Home/view.js
@@ -2,12 +2,15 @@ import React from "react";
 
 //styles
 import "./styles.css"; 
+//import "./styles_responsive.css";
+
 //data
 import data from "../../data/infoAPI.json";
 //components
 import {InfoArea} from "../../components/InfoArea";
 import {EndPointsArea} from "../../components/EndPointsArea";
 import {UrlParametersArea} from "../../components/UrlParametersArea";
+import { DataSchema } from "../../components/DataSchema";
 
 export default function(props){
     return(
@@ -15,7 +18,13 @@ export default function(props){
             {console.log(data)}
             <InfoArea title={data.title} description={data.description}/>
             <EndPointsArea endpoints={data.end_points}/>
-            <UrlParametersArea urlParameters={data.url_parameters}/>
+            <UrlParametersArea urlParameters={data.url_parameters}/>            
+            <div className="ctr-data-schema">
+                <h5 className="subtitle">DATA SCHEMAS</h5>
+                <hr/>
+                <DataSchema title="FULL DATA SCHEMA" object={data.data_schema.full_response}/>
+                <DataSchema title="SIMPLIFIED DATA SCHEMA" object={data.data_schema.simplified_response}/>
+            </div>
         </div>
     );
 }
